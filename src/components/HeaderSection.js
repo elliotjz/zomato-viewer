3f6de8013d367d84128f2400ae7b552b480ae3ac
Elliot Zoerner <eliot.zoerner@gmail.com>
2019-09-19 15:00:35 +0930

    Lifted state

--- src/components/HeaderSection.js
+++ src/components/HeaderSection.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 import Checkbox from './Checkbox';
 import Slider from './Slider';
@@ -49,186 +50,93 @@ const Container = styled.div`
   }
 `;
 
-const categories = {
-  Dining: { text: 'Dining', id: 2 },
-  'Take-Away': { text: 'Take-Away', id: 5 },
-  Delivery: { text: 'Delivery', id: 1 },
-  'Pubs-And-Bars': { text: 'Pubs & Bars', id: 11 },
-};
-
-const cuisines = {
-  'Cafe-Food': { text: 'Cafe Food', id: 1039 },
-  'Coffee-and-Tea': { text: 'Coffee and Tea', id: 161 },
-  Pizza: { text: 'Pizza', id: 82 },
-  'Fast-Food': { text: 'Fast Food', id: 40 },
-  Asian: { text: 'Asian', id: 3 },
-  Bakery: { text: 'Bakery', id: 5 },
-  Italian: { text: 'Italian', id: 55 },
-  Sandwich: { text: 'Sandwich', id: 304 },
-  Chinese: { text: 'Chinese', id: 25 },
-  'Pub-Food': { text: 'Pub Food', id: 983 },
-  Other: { text: 'Other', id: 110 },
-};
-
-class HeaderSection extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      checkboxes: {},
-      ratings: [1, 2],
-      costs: [1, 2],
-    };
-  }
-
-  componentDidMount() {
-    const checkboxes = {};
-    const categoryKeys = Object.keys(categories);
-    const cuisineKeys = Object.keys(cuisines);
-    [...categoryKeys, ...cuisineKeys].forEach((item) => {
-      checkboxes[item] = false;
-    });
-    this.setState({ checkboxes });
-  }
-
-  async componentDidUpdate() {
-    const { checkboxes } = this.state;
-    let query = '?entity_id=297&entity_type=city';
-
-    // Append category queries
-    let categoryQuery = '&category=';
-    Object.keys(categories).forEach((item) => {
-      if (checkboxes[item]) {
-        categoryQuery += `${categories[item].id},`;
-      }
-    });
-    if (categoryQuery !== '&category=') {
-      query += categoryQuery.slice(0, -1);
-    }
-
-    // Append cuisine queries
-    let cuisineQuery = '&cuisine=';
-    Object.keys(cuisines).forEach((item) => {
-      if (checkboxes[item]) {
-        cuisineQuery += `${cuisines[item].id},`;
-      }
-    });
-    if (cuisineQuery !== '&cuisine=') {
-      query += cuisineQuery.slice(0, -1);
-    }
-
-    const url = `https://developers.zomato.com/api/v2.1/search${query}`;
-    const res = await fetch(url, {
-      headers: {
-        Accept: 'application/json',
-        'user-key': `${process.env.REACT_APP_ZOMATO_API_KEY}a`,
-      },
-    });
-    if (res.status === 200) {
-      const resJson = await res.json();
-      console.log(resJson);
-    } else {
-      console.log(`Error. State: ${res.status}`);
-    }
-  }
-
-  onCheckboxChange = (e) => {
-    const { checkboxes } = this.state;
-    const { target } = e;
-    const { checked, value } = target;
-
-    this.setState({
-      checkboxes: {
-        ...checkboxes,
-        [value]: checked,
-      },
-    });
-  }
-
-  onRatingsChange = (values) => {
-    const intValues = values.map((v) => parseInt(v, 10));
-    this.setState({
-      ratings: intValues,
-    });
-  }
-
-  onCostsChange = (values) => {
-    const intValues = values.map((v) => parseInt(v, 10));
-    this.setState({
-      costs: intValues,
-    });
-  }
-
-  render() {
-    const { checkboxes, ratings, costs } = this.state;
-    return (
-      <Container>
-        <form>
-          <div className="checkboxes-container">
-            <fieldset id="categories">
-              <label className="section-label" htmlFor="categories">CATEGORY</label>
-              <div className="checkboxes">
-                {Object.keys(categories).map((category) => (
-                  <Checkbox
-                    key={category}
-                    name={category}
-                    text={categories[category].text}
-                    categoryName="category"
-                    checked={!!checkboxes[category]}
-                    onChange={this.onCheckboxChange}
-                  />
-                ))}
-              </div>
-            </fieldset>
-            <fieldset id="cuisines">
-              <label className="section-label" htmlFor="cuisines">CUISINE</label>
-              <div className="checkboxes">
-                {Object.keys(cuisines).map((cuisine) => (
-                  <Checkbox
-                    key={cuisine}
-                    name={cuisine}
-                    text={cuisines[cuisine].text}
-                    categoryName="cuisine"
-                    checked={!!checkboxes[cuisine]}
-                    onChange={this.onCheckboxChange}
-                  />
-                ))}
-              </div>
-            </fieldset>
+const HeaderSection = ({
+  categories,
+  cuisines,
+  checkboxes,
+  ratings,
+  costs,
+  onRatingsChange,
+  onCostsChange,
+  onCheckboxChange,
+}) => (
+  <Container>
+    <form>
+      <div className="checkboxes-container">
+        <fieldset id="categories">
+          <label className="section-label" htmlFor="categories">CATEGORY</label>
+          <div className="checkboxes">
+            {Object.keys(categories).map((category) => (
+              <Checkbox
+                key={category}
+                name={category}
+                text={categories[category].text}
+                categoryName="category"
+                checked={!!checkboxes[category]}
+                onChange={onCheckboxChange}
+              />
+            ))}
           </div>
-          <div className="sliders-container">
-            <fieldset>
-              <label className="section-label" htmlFor="rating">
-                RATING
-                <br />
-                <Slider
-                  values={ratings}
-                  min={0}
-                  max={5}
-                  minText="0"
-                  maxText="5"
-                  onChange={this.onRatingsChange}
-                />
-              </label>
-            </fieldset>
-            <fieldset>
-              <label className="section-label" htmlFor="cost">
-                COST
-                <br />
-                <Slider
-                  values={costs}
-                  min={0}
-                  max={3}
-                  minText="$"
-                  maxText="$$$$"
-                  onChange={this.onCostsChange}
-                />
-              </label>
-            </fieldset>
+        </fieldset>
+        <fieldset id="cuisines">
+          <label className="section-label" htmlFor="cuisines">CUISINE</label>
+          <div className="checkboxes">
+            {Object.keys(cuisines).map((cuisine) => (
+              <Checkbox
+                key={cuisine}
+                name={cuisine}
+                text={cuisines[cuisine].text}
+                categoryName="cuisine"
+                checked={!!checkboxes[cuisine]}
+                onChange={onCheckboxChange}
+              />
+            ))}
           </div>
-        </form>
-      </Container>
-    );
-  }
-}
+        </fieldset>
+      </div>
+      <div className="sliders-container">
+        <fieldset>
+          <label className="section-label" htmlFor="rating">
+            RATING
+            <br />
+            <Slider
+              values={ratings}
+              min={0}
+              max={5}
+              minText="0"
+              maxText="5"
+              onChange={onRatingsChange}
+            />
+          </label>
+        </fieldset>
+        <fieldset>
+          <label className="section-label" htmlFor="cost">
+            COST
+            <br />
+            <Slider
+              values={costs}
+              min={0}
+              max={3}
+              minText="$"
+              maxText="$$$$"
+              onChange={onCostsChange}
+            />
+          </label>
+        </fieldset>
+      </div>
+    </form>
+  </Container>
+);
+
+HeaderSection.propTypes = {
+  categories: PropTypes.objectOf(PropTypes.object).isRequired,
+  cuisines: PropTypes.objectOf(PropTypes.object).isRequired,
+  checkboxes: PropTypes.objectOf(PropTypes.bool).isRequired,
+  ratings: PropTypes.arrayOf(PropTypes.number).isRequired,
+  costs: PropTypes.arrayOf(PropTypes.number).isRequired,
+  onRatingsChange: PropTypes.func.isRequired,
+  onCostsChange: PropTypes.func.isRequired,
+  onCheckboxChange: PropTypes.func.isRequired,
+};
 
 export default HeaderSection;
--- src/components/ResultsSection.js
+++ src/components/ResultsSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -9,8 +10,15 @@ const Container = styled.div`
 
 const ResultsSection = ({ restaurants }) => (
   <Container>
-    <p>ResultsSection</p>
+    <h3>ResultsSection</h3>
+    {restaurants.map((r) => (
+      <p key={r.restaurant.R.res_id}>{r.restaurant.name}</p>
+    ))}
   </Container>
 );
+
+ResultsSection.propTypes = {
+  restaurants: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default ResultsSection;
